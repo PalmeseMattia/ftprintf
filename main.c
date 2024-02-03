216256b4ffac7db404a84c8f3c5b7287bcf14c0c
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-02-03 11:33:30 +0100

    refactoring

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int manage_conversion(char *conversion, va_list arguments);
 
 int main()
 {
-	ft_printf("lo zio %c %s zia\n",'e', "la bella");
+	ft_printf("lo zio%c%c %s zia\n",',','e',"la bella");
 	return 0;
 }
 
@@ -24,23 +24,24 @@ int ft_printf(const char *format, ...)
 	va_start(arguments, format);
 	while ((next_stop = ft_strchr(format,'%')) != NULL)
 	{
-		chars_printed += ft_putnstr_fd((char *)format, next_stop - format, 1);// Search for the next % character
+		chars_printed += ft_putnstr_fd((char *)format, next_stop - format, 1);
 		chars_printed += manage_conversion(next_stop + CHAR_SIZE, arguments);
-		format = next_stop + (2 * CHAR_SIZE); // TODO:Test in case of two %% adiacent or %d%d for example
+		// TODO:Test in case of two %% adiacent or %d%d for example
+		format = next_stop + (2 * CHAR_SIZE);
 	}
 	chars_printed += ft_putstr_fd((char *)format, 1);
 	va_end(arguments);
 	return (chars_printed);
 }
 
-/**
+/*
  * According to the C language specification, usual arithmetic conversions are applied 
  * to the unnamed arguments of variadic functions. 
  * Hence, any integer type shorter than int (e. g. bool, char and short) 
  * are implicitly converted int; float is promoted to double, etc.
  * Do what the compiler asks you for (use va_arg() with int as its second parameter), 
  * else your code will invoke undefined behavior.
-*/
+ */
 int manage_conversion(char *conversion, va_list arguments)
 {
 	if (*conversion == 'c')
