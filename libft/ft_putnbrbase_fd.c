0823f17d0fee2371e8b6bee483235749f922f14f
Davide Mattia Palmese <dpalmese@e3r5p7.42roma.it>
2024-02-12 13:07:57 +0100

    non destructive norm

--- libft/ft_putnbrbase_fd.c
+++ libft/ft_putnbrbase_fd.c
@@ -1,9 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbrbase_fd.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/12 13:03:53 by dpalmese          #+#    #+#             */
+/*   Updated: 2024/02/12 13:05:39 by dpalmese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 
 int	ft_putnbrbase_fd(long number, int base, int fd, int capital)
 {
-	char *digits;
-	int result;
+	char	*digits;
+	int		result;
 
 	result = 0;
 	if (capital)
@@ -16,16 +27,12 @@ int	ft_putnbrbase_fd(long number, int base, int fd, int capital)
 		return (0);
 	if (number < 0 && base == 10)
 	{
-		ft_putchar_fd('-', fd);
+		result += ft_putchar_fd('-', fd);
 		number = -number;
-		result += 1;
 	}
 	if (number < base)
 	{
-		if (capital)
-			result += ft_putchar_fd(digits[number], fd);
-		else
-			result += ft_putchar_fd(digits[number], fd);
+		result += ft_putchar_fd(digits[number], fd);
 		return (result);
 	}
 	result += ft_putnbrbase_fd(number / base, base, fd, capital);
