42d522de1a731bf5c3e3f9880aeba0ec170ecea8
Davide Mattia Palmese <dpalmese@e3r5p7.42roma.it>
2024-02-12 12:56:08 +0100

    non destructive norm

--- ft_printf.h
+++ ft_printf.h
@@ -1,12 +1,23 @@
-#ifndef FTPRINTF_H
-#define FTPRINTF_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/12 12:53:00 by dpalmese          #+#    #+#             */
+/*   Updated: 2024/02/12 12:54:03 by dpalmese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-#include <stdarg.h>
-#include "libft/libft.h"
+# include <stdarg.h>
+# include "libft/libft.h"
 
-#define CHAR_SIZE CHAR_BIT/8
+# define CHAR_SIZE 1
 
 int	ft_printf(const char *format, ...);
-int ft_manage_conversion(char *conversion, va_list arguments);
+int	ft_manage_conversion(char *conversion, va_list arguments);
 
 #endif
