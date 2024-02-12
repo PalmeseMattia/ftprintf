5c7dd1069abbe59bae082a04cf8b1beb4aba35dd
Davide Mattia Palmese <dpalmese@e3r5p7.42roma.it>
2024-02-12 13:15:17 +0100

    norm

--- libft/ft_putpointer_fd.c
+++ libft/ft_putpointer_fd.c
@@ -6,12 +6,10 @@
 /*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 17:54:21 by dpalmese          #+#    #+#             */
-/*   Updated: 2024/02/12 12:41:11 by dpalmese         ###   ########.fr       */
+/*   Updated: 2024/02/12 13:12:25 by dpalmese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-
-#define MASK(data, shift) (data >> (shift * 4) & 0xF)
 
 /*
  * Write the address pointed by p to the file descriptor fd,
@@ -24,21 +22,21 @@ int	ft_putpointer_fd(void *p, int fd)
 	int					result;
 	unsigned char		byte;
 
-	if(!p)
+	if (!p)
 		return (ft_putstr_fd("0x0", fd));
 	result = 0;
 	i = (sizeof(p) * 2) - 1;
 	p_intvalue = (uintptr_t) p;
 	result += ft_putstr_fd("0x", fd);
-	byte = MASK(p_intvalue, i);
+	byte = (p_intvalue >> (i * 4) & 0xF);
 	while (i >= 0 && byte == 0)
 	{
 		i--;
-		byte = MASK(p_intvalue, i);
+		byte = (p_intvalue >> (i * 4) & 0xF);
 	}
 	while (i >= 0)
 	{
-		byte = MASK(p_intvalue, i);
+		byte = (p_intvalue >> (i * 4) & 0xF);
 		result += ft_putnbrbase_fd((long)byte, 16, 1, 0);
 		i--;
 	}
